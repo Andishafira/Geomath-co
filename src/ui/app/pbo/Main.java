e9711cdc31bde34a1283d65a5dcf2de20c5b4588
Andishafira <andishafira757@gmail.com>
2019-05-21 15:38:29 +0700

    UI

--- src/ui/app/pbo/Main.java
+++ src/ui/app/pbo/Main.java
@@ -27,28 +27,43 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jToggleButton1 = new javax.swing.JToggleButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(290, 250, 30, 0);
 
-        jToggleButton1.setText("jToggleButton1");
+        jLabel3.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 51, 51));
+        jLabel3.setText("Amoba");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(220, 50, 210, 110);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(573, Short.MAX_VALUE)
-                .addComponent(jToggleButton1)
-                .addGap(539, 539, 539))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(264, 264, 264)
-                .addComponent(jToggleButton1)
-                .addContainerGap(337, Short.MAX_VALUE))
-        );
+        jLabel4.setFont(new java.awt.Font("Android Nation", 1, 100)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 51, 51));
+        jLabel4.setText("GEOMETRY");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(200, 80, 1100, 210);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gambar/startIcon.png"))); // NOI18N
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(530, 520, 310, 230);
+
+        jLabel7.setFont(new java.awt.Font("FREEDOM", 1, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(0, 51, 51));
+        jLabel7.setText("Develop by : Faza A. , Annisa F.N. , Andi Shafira D.K.");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(580, 230, 760, 60);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gambar/abstract-blue-geometric-shapes-background-vector.jpg"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(-150, -60, 1490, 910);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -89,6 +104,11 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     // End of variables declaration//GEN-END:variables
 }
