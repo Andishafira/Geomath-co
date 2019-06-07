6501e36c6139534a669ca54db597a802b84904d8
annisafitria <annisafitria2102@gmail.com>
2019-06-07 12:45:37 +0700

    UI

--- src/bangunDatar/belahKetupatLatihanSoal2.java
+++ src/bangunDatar/belahKetupatLatihanSoal2.java
@@ -27,36 +27,20 @@ public class belahKetupatLatihanSoal2 extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel5 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jTextField2 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(24, 64, 105));
         setLayout(null);
-
-        jLabel5.setFont(new java.awt.Font("DialogInput", 0, 24)); // NOI18N
-        jLabel5.setText("Diketahui 2222keliling sebuah belah ketupat ialah 32 cm,");
-        add(jLabel5);
-        jLabel5.setBounds(110, 40, 830, 70);
-
-        jLabel4.setFont(new java.awt.Font("DialogInput", 0, 24)); // NOI18N
-        jLabel4.setText("maka berapa panjang masing masing sisi belah ketupat tesebut? ");
-        add(jLabel4);
-        jLabel4.setBounds(40, 100, 900, 70);
-
-        jLabel6.setFont(new java.awt.Font("DialogInput", 2, 24)); // NOI18N
-        jLabel6.setText("Jawab : ");
-        add(jLabel6);
-        jLabel6.setBounds(40, 220, 130, 30);
         add(jTextField1);
-        jTextField1.setBounds(180, 210, 290, 60);
+        jTextField1.setBounds(790, 90, 130, 40);
 
-        jButton1.setText("Cek");
+        jButton1.setText("CEK");
         add(jButton1);
-        jButton1.setBounds(510, 220, 77, 32);
+        jButton1.setBounds(820, 160, 77, 23);
 
         jTextField2.setEditable(false);
         jTextField2.addActionListener(new java.awt.event.ActionListener() {
@@ -65,7 +49,15 @@ public class belahKetupatLatihanSoal2 extends javax.swing.JPanel {
             }
         });
         add(jTextField2);
-        jTextField2.setBounds(630, 210, 280, 60);
+        jTextField2.setBounds(790, 260, 130, 40);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gambar/LATIHAN SOAL 2.png"))); // NOI18N
+        add(jLabel1);
+        jLabel1.setBounds(0, 0, 720, 340);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gambar/jawaban.png"))); // NOI18N
+        add(jLabel2);
+        jLabel2.setBounds(680, 0, 350, 340);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
@@ -75,9 +67,8 @@ public class belahKetupatLatihanSoal2 extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
